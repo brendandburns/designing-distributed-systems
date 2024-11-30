e8a80909c3a73546ec349e117e183a2c8554b7c3
Brendan Burns <5751682+brendandburns@users.noreply.github.com>
2024-11-30 12:33:29 -0800

    Update mysql-health.go

--- adapters/mysql-health.go
+++ adapters/mysql-health.go
@@ -14,23 +14,22 @@ var (
 	passwd = flag.String("password", "", "The database password")
 	db     = flag.String("database", "", "The database to connect to")
 	query  = flag.String("query", "", "The test query")
-	addr   = flag.String("address", "localhost:8080", "The address to listen on")
+	addr   = flag.String("address", "localhost:8080",
+		"The address to listen on")
 )
 
 // Basic usage:
-//   db-check --query="SELECT * from my-cool-table" \
-//            --user=bdburns \
-//            --passwd="you wish"
-//
+// db-check --query="SELECT * from my-cool-table" \
+// --user=bdburns \
+// --passwd="you wish"
 func main() {
 	flag.Parse()
-	db, err := sql.Open("localhost", fmt.Sprintf("%s:%s@/%s", *user, *passwd, *db))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@/%s", *user, *passwd, *db))
 	if err != nil {
 		fmt.Printf("Error opening database: %v", err)
 	}
-
 	// Simple web handler that runs the query
-	http.HandleFunc("", func(res http.ResponseWriter, req *http.Request) {
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		_, err := db.Exec(*query)
 		if err != nil {
 			res.WriteHeader(http.StatusInternalServerError)
